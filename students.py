823ad35bb6e37f4e73b31167dcb675e0067315e9
LIN Xiaoqian <xiaoqian.lin@connect.polyu.hk>
2024-09-15 05:27:45 +0000

    add simple python programs

---
+++ students.py
@@ -0,0 +1,16 @@
+
+import json
+
+# 读取 JSON 文件
+with open('student.json', 'r') as file:
+    data = json.load(file)
+
+# 获取学生列表
+students = data['students']
+
+# 列出所有学生
+for student in students:
+    print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}")
+
+#print the second student's name
+print(students[1]['name'])
